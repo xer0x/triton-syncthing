d9e5d7165c46a4f6dc0cd6a3d4475f190ed8208c
Drew Miller <drew666@gmail.com>
2015-09-03 12:47:18 -0700

    get web url via consul_env.sh

--- consul_env.sh
+++ consul_env.sh
@@ -5,4 +5,4 @@ consul=consul
 export STUSERNAME=$(curl -L -s -f http://$consul:8500/v1/kv/syncthing/username | json -aH Value | base64 --decode)
 export STPASSWORD=$(curl -L -s -f http://$consul:8500/v1/kv/syncthing/password | json -aH Value | base64 --decode)
 export MYIPPUBLIC=$(ip addr show eth1 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
-export WEB_URL="http://$MYIPPUBLIC:8384"
+export WEB_URL="http://$STUSERNAME:$STPASSWORD@$MYIPPUBLIC:8384"
