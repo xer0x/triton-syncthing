db5ebde4be4f4e4bed1653bd8aa68394e5a526e1
Drew Miller <drew666@gmail.com>
2015-09-04 23:34:54 -0700

    Helpful comments based on Tims feedback

--- setup.sh
+++ setup.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
-# TODO: do we need to export STUSERNAME and STPASSWORD ?
+#
+# Setup Syncthing to connect to a mesh of devices.
+#
+# Overview:
+#
+# We use Consul to store both the list of devices, and a shared username and password for all the devices.
+#
+# If this is the first Syncthing device to talk to Consul, then we choose a random authentication values. We register ourselves with Consul as a running service.
+#
+# Otherwise, we use the authentication values in Consul. Next we ask Consul for the IP address of another running Syncthing device. We connect to this Syncthing with "Introducer mode" enabled.
+#
+# "Introducer mode" is the special magic that will keep our mesh network of containers linked together. When "Introducer mode" is enabled the device will tell us about all the other devices that it knows about. It will also tell us about any new devices that it learns about.
+#
+# I feel that Syncthing's UI and easy clustering with "Introducer mode" give it the edge over other tools that we could have used like Rsync, Unison, or Bittorrent-Sync.
+#
+# We use syncthing-cli to update each device's configuration.
+#
 
-# TODO deregister containers from Consul when they are destroyed
+
+# TODO healthcheck and deregister containers from Consul when they are destroyed
+
 
 #set -o xtrace
 
@@ -27,12 +45,12 @@ function getConfiguration {
   echo '# Checking Consul for configuration'
   echo '#'
 
-  # TODO: Wait for consul to be available ?
-
-  INTRODUCER_IP=$(curl --retry 4 --retry-delay 2 -L -s -f http://$consul:8500/v1/catalog/service/syncthing | json -aH ServiceAddress | head -1)
+  INTRODUCER_IP=$(curl --retry 6 --retry-delay 2 -L -s -f http://$consul:8500/v1/catalog/service/syncthing | json -aH ServiceAddress | head -1)
   if [ -n "$INTRODUCER_IP" ]; then
+    echo '# Using existing authentication keys from Consul'
     getAuthKeys
   else
+    echo '# Adding new authentication keys to Consul'
     setAuthKeys
   fi
 }
@@ -53,6 +71,8 @@ function register_service_with_consul {
     touch /setup.finished
   else
     echo '# Error: unable to register container with Consul'
+    echo '#'
+    echo 'Will retry on next container restart.'
     exit 1
   fi
 }
@@ -71,8 +91,11 @@ function wait_for_consul {
     echo '#'
     curl http://$consul:8500/v1/agent/services
     echo
-    echo '# TODO: kill setup.sh pid'
-    exit
+    echo '#'
+    echo '# Aborting setup and exiting Container because Consul is unreachable'
+    echo '#'
+    kill $PPID
+    exit 1
   else
     echo '# Consul instance found and responsive'
     echo '#'
@@ -82,7 +105,7 @@ function wait_for_consul {
 function wait_for_syncthing {
   #echo -n 'Waiting..'
   local hostname=$1
-  echo "Waiting for API be up on ${hostname}..."
+  echo "# Waiting for API be up on ${hostname}..."
   local ST_RESPONSIVE=0
   while [ $ST_RESPONSIVE != 1 ]; do
     #echo -n '.'
@@ -93,12 +116,12 @@ function wait_for_syncthing {
       sleep 1.3
     fi
   done
-  echo "API ready."
+  echo "# API ready."
 }
 
 function abort_if_setup {
   if [ -e /setup.finished ]; then
-    echo Setup aborting. Already completed.
+    echo '# Setup aborting. Already completed.'
     exit
   fi
 }
@@ -109,23 +132,24 @@ function configure {
   syncthing-cli gui set user "$STUSERNAME"
   syncthing-cli gui set password "$STPASSWORD"
   syncthing-cli gui set address "0.0.0.0:8384"
-  syncthing-cli devices set $MY_ID introducer true
-  # TODO set syncthing name to docker_container name (not the current hash id?)
+  # TODO set syncthing name to docker_container name
+
   MY_ID=$(syncthing-cli id)
+  syncthing-cli devices set $MY_ID introducer true
 
   if [ -n "$INTRODUCER_IP" ]; then
-    # WARNING: not everything responds to ping
-    #ping -c1 -q $INTRODUCER_IP > /dev/null 2>&1
-    #if [ $? == 0 ]; then
-    if [ 1 == 1 ]; then
+    ping -c1 -q $INTRODUCER_IP > /dev/null 2>&1
+    if [ $? == 0 ]; then
       wait_for_syncthing $INTRODUCER_IP
       echo "#"
-      echo "# Introducing new device to Syncthing cluster"
+      echo "# Adding new device to Syncthing cluster using Syncthing:${INTRODUCER_ID} as an Introducer device"
       echo "#"
+      # Syncthing-cli uses the env vars STUSERNAME/STPASSWORD to authenticate
       INTRODUCER_ID=$(syncthing-cli --endpoint "http://${INTRODUCER_IP}:8384" id)
       syncthing-cli devices add $INTRODUCER_ID
       syncthing-cli --endpoint "http://$INTRODUCER_IP:8384" devices add $MY_ID
       syncthing-cli devices set $INTRODUCER_ID introducer true
+      echo "# Enabling sync for 'default' folder"
       syncthing-cli folders devices add default $INTRODUCER_ID
       syncthing-cli --endpoint "http://$INTRODUCER_IP:8384" folders devices add default $MY_ID
       echo "# Introducer status"
@@ -134,7 +158,7 @@ function configure {
       syncthing-cli --endpoint "http://$INTRODUCER_IP:8384" restart
     else
       echo "Introducer IP ${INTRODUCER_IP} is not reachable."
-      echo "TODO: deregister this missing introducer"
+      exit 1
     fi
   else
     echo "#"
